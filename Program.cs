20aae8e88f373bab03b8f19c964fdfaba6b1dea1
Santosh Marigowda <santosharakere@gmail.com>
2020-03-16 20:27:32 +0000

    fibo number

--- Program.cs
+++ Program.cs
@@ -12,7 +12,8 @@ namespace BranchesAndLoops
             // DoWhileLoop();
             // ForLoop();
             // FindSum();
-            GenericList();
+            // GenericList();
+            fibNum();
         }
         static void ExploreIf()
         {
@@ -74,17 +75,36 @@ namespace BranchesAndLoops
             var names = new List<string> { "Santosh", "Roopa", "Sukruthi" };
             names.Add("Lingaraju");
             names.Sort();
-            foreach( var name in names) {
+            foreach (var name in names)
+            {
                 Console.WriteLine(name.ToUpper());
             }
             Console.WriteLine($"My name is {names[0]}");
             Console.WriteLine($"The list has {names.Count} names in it");
             var nameToSearch = "Bharathi";
             var index = names.IndexOf(nameToSearch);
-            if(index != -1) {
+            if (index != -1)
+            {
                 Console.WriteLine($"{nameToSearch} is in the list");
-            } else {
+            }
+            else
+            {
                 Console.WriteLine($"{nameToSearch} is not in the list");
+            }
+        }
+        static void fibNum()
+        {
+            var fibNumbers = new List<int> { 1, 1 };
+
+            for (var i = 3; i <= 20; i++)
+            {
+                var prev1 = fibNumbers[fibNumbers.Count - 1];
+                var prev2 = fibNumbers[fibNumbers.Count - 2];
+                fibNumbers.Add(prev1 + prev2);
+            }
+
+            foreach( var num in fibNumbers) {
+                Console.WriteLine(num);
             }
         }
     }
