7c04413d9d65919b6c73a2d2dc95f8cb95d1d6d1
Santosh Marigowda <santosharakere@gmail.com>
2020-03-16 17:02:58 +0000

    Branch using if. Loop usuing while

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace BranchesAndLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ExploreIf();
+            ExploreLoop();
+        }
+        static void ExploreIf()
+        {
+            int a = 5;
+            int b = 4;
+            if (a + b > 10)
+            {
+                Console.WriteLine("The answer is greater than 10");
+            }
+            else
+            {
+                Console.WriteLine("The answer is less than 10");
+            }
+        }
+
+        static void ExploreLoop() {
+            int counter = 0;
+            while (counter < 10) {
+                Console.WriteLine($"Hello World! Counter = {counter}");
+                counter++;
+            }
+        }
+    }
+}
